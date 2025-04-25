c0ab0b37f103eabea61fc15440e06fb6f5f5a23c
Propato <david123propato@gmail.com>
2025-04-25 01:15:01 -0300

    update backend host config

--- src/api/utils/config.ts
+++ src/api/utils/config.ts
@@ -1,11 +1,16 @@
 import type { IUser } from "@/utils/interfaces";
 import axios from "axios";
 
-const BACKEND_URL = import.meta.env.VITE_APP_BACKEND_URL;
+const isLocalhost = ["localhost", "127.0.0.1"].includes(window.location.hostname);
+
+const BACKEND_URL = isLocalhost
+    ? `http://localhost:${import.meta.env.VITE_APP_BACKEND_PORT}`
+    : import.meta.env.VITE_APP_BACKEND_URL;
+
 const TIMEOUT = Number(import.meta.env.VITE_APP_BACK_END_TIMEOUT) || 10000;
 
 if (!BACKEND_URL) {
-    throw new Error("Variável de ambiente BACKEND_URL não definida");
+    throw new Error("BACKEND_URL não está definida corretamente.");
 }
 
 const instance = axios.create({
@@ -14,9 +19,7 @@ const instance = axios.create({
 });
 
 instance.interceptors.request.use((config) => {
-    const { token }: { user: IUser; token: string } = JSON.parse(
-        localStorage.getItem("user") || "",
-    );
+    const { token }: { user: IUser; token: string } = JSON.parse(localStorage.getItem("user") || "");
     if (token) {
         config.headers.Authorization = `Token ${token}`;
     }
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
 import { fileURLToPath, URL } from "node:url";
-
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueDevTools from "vite-plugin-vue-devtools";
 
-export default defineConfig(() => {
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), "");
+    const allowedHosts = env.VITE_APP_ALLOWED_HOSTS?.split(",") ?? [];
+
+    console.log("allowedHosts: ", allowedHosts);
+
     return {
         plugins: [vue(), vueDevTools()],
         resolve: {
@@ -14,6 +18,7 @@ export default defineConfig(() => {
         },
         server: {
             port: 3000,
+            allowedHosts,
         },
     };
 });
